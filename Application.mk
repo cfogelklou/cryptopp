6d8128dad47f16dbb8c8169df0c7767bc78754a6
cfogelklou <chris.fogelklou@gmail.com>
2015-09-04 09:48:24 +0200

    Can build using standard JNI ndk-build command!

--- Application.mk
+++ Application.mk
@@ -1,10 +1,12 @@
 # Build both ARMv5TE and ARMv7-A machine code.
 
+APP_PLATFORM    := android-18
 APP_STL := gnustl_static
 APP_CPPFLAGS += -fexceptions -frtti
 
 APP_ABI := all
 #APP_ABI := armeabi armeabi-v7a arm64-v8a x86 x86_64 mips mips64
+#APP_ABI := armeabi-v7a
 
 # When profiling, just build the arm architectures.
 #APP_ABI := armeabi armeabi-v7a
