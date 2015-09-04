f665bd6a38005f5b11967264fd8fa6d0179076f2
cfogelklou <chris.fogelklou@gmail.com>
2015-09-04 10:04:18 +0200

    Build "app" for android and upload binaries.

--- build_android.sh
+++ build_android.sh
@@ -4,10 +4,12 @@ if [[ $pwd == *"jni"* ]]
 then
   echo "ok, we are in jni."
 else
+  echo "create symbolic link to this directory as jni"
   ln -s . jni
   cd jni  
 fi
 rm -rf ../obj
-ndk-build jni
+ndk-build
+
 
 
