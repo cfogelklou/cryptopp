95e9fa156a46e7e9776939c5328a8c9bfdcc8632
cfogelklou <chris.fogelklou@gmail.com>
2015-09-04 10:33:16 +0200

    Static build for ARM is working.

--- Application.mk
+++ Application.mk
@@ -4,9 +4,9 @@ APP_PLATFORM    := android-18
 APP_STL := gnustl_static
 APP_CPPFLAGS += -fexceptions -frtti
 
-APP_ABI := all
+#APP_ABI := all
 #APP_ABI := armeabi armeabi-v7a arm64-v8a x86 x86_64 mips mips64
-#APP_ABI := armeabi-v7a
+APP_ABI := armeabi-v7a armeabi x86
 
 # When profiling, just build the arm architectures.
 #APP_ABI := armeabi armeabi-v7a
