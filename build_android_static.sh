59d3d5ab72d10479bd1e1d1431e65c64663bae0e
cfogelklou <chris.fogelklou@gmail.com>
2015-09-04 10:34:10 +0200

    This is the version with working android armeabi build.

--- build_android_static.sh
+++ build_android_static.sh
@@ -2,24 +2,6 @@
 export IS_IOS=0
 export IS_ANDROID=1
 
-#--------------------------
-# Build ARMEABI variant.
-#--------------------------
-export ANDROID_ARCH=x86
-export ANDROID_FLAGS="-D__ANDROID__"
-
-# Setup the environment
-. ./setenv-android.sh
-export ANDROID_STL_INC=$ANDROID_NDK_ROOT/sources/cxx-stl/gnu-libstdc++/4.9/include/
-export CXXFLAGS=-I$ANDROID_NDK_ROOT/sources/cxx-stl/gnu-libstdc++/4.9/libs/$ANDROID_ARCH/include
-make -f GNUmakefile-cross clean
-make -f GNUmakefile-cross static
-
-# Copy build library into a subdirectory.
-rm -rf $ANDROID_ARCH
-mkdir $ANDROID_ARCH
-cp *.a $ANDROID_ARCH/
-
 
 #--------------------------
 # Build ARMEABI variant.
